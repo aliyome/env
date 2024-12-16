40127fabdcbc4e23f9fe8d574e68dd2fea3523d7
aliyome <aliyome@gmail.com>
2024-12-17 03:07:46 +0900

    Add streamdeck

--- setup.sh
+++ setup.sh
@@ -55,6 +55,7 @@ brew install logi-options+
 brew install obs
 brew install vlc
 brew install the-unarchiver
+brew install elgato-stream-deck
 brew install --cask warp   # --cask を省略できるが warp という名前の Formula が追加されたらつらいので明示的につける
 brew install --cask docker # --cask をつけると docker ランタイムだけでなく Docker Desktop CE がインストールされる
 brew install --cask tailscale # --cask をつけると tailscale ランタイムだけでなく App がインストールされる
